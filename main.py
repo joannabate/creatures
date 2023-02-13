b265c7b0f1943470bf4fcd64b696746a8bb9483d
Jon Bate <jonbate@jons-mbp-2.gateway.sonic.net>
2023-02-12 23:44:15 -0800

    changed data format to sunrise/sunset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
+from pytz import timezone
 from time import time, sleep
 import mido
 import multiprocessing as mp
@@ -51,24 +52,42 @@ class Audio:
 
 class Data:
     def __init__(self):
-
-        self.df = pd.read_csv('power_data.csv', index_col=0)
-        self.df['timestamp'] = pd.to_datetime(self.df.index, unit='s', utc=True).tz_convert("US/Hawaii")
-        self.df['date'] = self.df['timestamp'].dt.date
-        self.df['hour'] = self.df['timestamp'].dt.hour
-
-        self.df[['power.ams_solar', 'power.pre_battery_grid', 'power.grid']
-            ] = self.df.groupby(['service_account', 'date'])[
-                'power.ams_solar', 'power.pre_battery_grid', 'power.grid'].apply(
-                    lambda x: (x - x.min())/(x.max() - x.min()))
-
-        self.dfs = {}
-        for col in ['power.ams_solar', 'power.pre_battery_grid', 'power.grid']:
-            self.dfs[col] = self.df.pivot(index=['date', 'hour', 'timestamp'], columns='service_account', values=col).reset_index()
-
-
+        self.df = self.process_data()
+        
         # Setup audio
         self.outport = mido.open_output('IAC Driver creatures')
+
+    def process_data(self):
+
+        # Read in data and drop unecessary columns
+        df = pd.read_csv('sunrise_sunset_times.csv')
+        df.drop(columns=['Unnamed: 4', 'Daylength'], inplace=True)
+
+        # Convert sunrise and sunset times to datetime objects in Pacfic winter time
+        df['Sunrise'] = pd.to_datetime(df['Date'] + ' ' + df['Sunrise']).dt.tz_localize('US/Pacific').dt.tz_convert('Etc/GMT+8')
+        df['Sunset'] = pd.to_datetime(df['Date'] + ' ' + df['Sunset']).dt.tz_localize('US/Pacific').dt.tz_convert('Etc/GMT+8')
+        
+        # Add in twilight start and end times
+        df['Twilight Start'] = df['Sunrise'] - timedelta(hours=1, minutes=30)
+        df['Twilight End'] = df['Sunset'] + timedelta(hours=1, minutes=30)
+
+        df['Date'] = pd.to_datetime(df['Date']).dt.tz_localize('US/Pacific').dt.tz_convert('Etc/GMT+8')
+
+
+        return df
+
+    def get_data_start_time(self):
+
+        start_time = datetime.now()
+        decimal_time = int(start_time.hour)/24 + int(start_time.minute)/(24*60) + int(start_time.second)/(24*60*60)
+
+        first_date = datetime(year=2022, month=12, day=21)
+        data_duration = (365*24*60*60)
+
+        data_start_time = first_date.timestamp() + (decimal_time * data_duration)
+
+        return datetime.fromtimestamp(data_start_time, timezone('Etc/GMT+8'))
+
 
     def convert_to_color(self, num):
         red = 0.7539, 0.2746
@@ -82,52 +101,76 @@ class Data:
 
         return {'x':x, 'y':y}
 
-    def get_current_timestamp(self, tz_offset=-7):
-        # Returns the timestep of the timeseries data based on current time of day
-        local_time = ((time() + (tz_offset * 60 * 60)) % SECONDS_IN_DAY)
-        current_timestamp = FIRST_TIMESTAMP + SECONDS_IN_YEAR * local_time/SECONDS_IN_DAY
-        current_timestamp = DATA_INTERVAL * int(current_timestamp/DATA_INTERVAL)
-        return current_timestamp
+    # def get_current_timestamp(self, tz_offset=-7):
+    #     # Returns the timestep of the timeseries data based on current time of day
+    #     local_time = ((time() + (tz_offset * 60 * 60)) % SECONDS_IN_DAY)
+    #     current_timestamp = FIRST_TIMESTAMP + SECONDS_IN_YEAR * local_time/SECONDS_IN_DAY
+    #     current_timestamp = DATA_INTERVAL * int(current_timestamp/DATA_INTERVAL)
+    #     return current_timestamp
 
     def run(self, current_timestamp):
 
-        starttime = time()
-
-        hour = -1
+        # Setup time counters
+        real_start_time = time()
+        data_time = self.get_data_start_time()
 
         while True:
-            for i, row in self.dfs['power.ams_solar'].iterrows():
-                print(row[0])
+            for i, row in self.df.iterrows():
+                # Skip forward to start of data
+                if row['Date'].date() == data_time.date():
+                    # We're on the right line for this date
+                    while row['Date'].date() == data_time.date():
+                        print(data_time)
+                        # While this is still true...
+                        if data_time < row['Twilight Start']:
+                            # Before Twilight it is dark
+                            val = 1
+                        elif data_time < row['Sunrise']:
+                            # Between twilight start and sunrise the sun is partially up
+                            # Figure out how far we are past twilight
+                            val = (row['Sunset'].timestamp() - data_time.timestamp()) / (60*90)
 
-                msg = mido.Message('note_on', note=MIDDLE_C, velocity=64)
-                self.outport.send(msg)
+                        elif data_time < row['Sunset']:
+                            # During the day it is light
+                            val = 0
 
-                cc_value = random.randint(1,100)
+                        elif data_time < row['Twilight End']:
+                            # Between sunset and twilight end the sun is partially up
+                            # Figure out how far we are past twilight
+                            val = (data_time.timestamp() - row['Sunset'].timestamp()) / (60*90)
 
-                msg = mido.Message('control_change', control=1, value=cc_value)
-                self.outport.send(msg)
-                print("Setting control signal %i to %i" % (1, cc_value))
+                        else:
+                            # After twilight end it is dark
+                            val = 1
 
-                msg = mido.Message('note_off', note=MIDDLE_C, velocity=64)
-                self.outport.send(msg)
+                        # Do stuff
+                        msg = mido.Message('note_on', note=MIDDLE_C, velocity=64)
+                        self.outport.send(msg)
 
-                color_1 = self.convert_to_color(row[0])
-                color_2 = self.convert_to_color(row[1])
-                print(row[0])
-                print(row[1])
-                if row['hour'] != hour:
-                    hour = row['hour']
-                    try:
-                        get_plugin('zigbee.mqtt').device_set(device='Bulb 1', property='color', value=color_1)
-                        get_plugin('zigbee.mqtt').device_set(device='Bulb 2', property='color', value=color_2)
-                        print('Setting Bulb 1 to %f', color_1)
-                        print('Setting Bulb 2 to %f', color_2)
+                        cc_value = random.randint(1,100)
 
-                    except:
-                        continue
+                        msg = mido.Message('control_change', control=1, value=cc_value)
+                        self.outport.send(msg)
+                        print("Setting control signal %i to %i" % (1, cc_value))
 
-                # Sleep until the next beat
-                sleep((TEMPO - ((time() - starttime) % TEMPO))/1000000.0)
+                        msg = mido.Message('note_off', note=MIDDLE_C, velocity=64)
+                        self.outport.send(msg)
+
+                        color = self.convert_to_color(val)
+                        try:
+                            get_plugin('zigbee.mqtt').device_set(device='Bulb 1', property='color', value=color)
+                            get_plugin('zigbee.mqtt').device_set(device='Bulb 2', property='color', value=color)
+                            print('Setting Bulb 1 to %f', color)
+                            print('Setting Bulb 2 to %f', color)
+
+                        except:
+                            continue
+
+                        # Advance data time
+                        data_time = data_time + timedelta(minutes=15)
+
+                        # Sleep until the next beat
+                        sleep((TEMPO - ((time() - real_start_time) % TEMPO))/1000000.0)
 
 
 if __name__ == "__main__":
