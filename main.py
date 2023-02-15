22c73184a5f6b23efd410a34390d57e3b3504b49
Jon Bate <jonbate@jons-mbp-2.gateway.sonic.net>
2023-02-15 00:01:29 -0800

    switched to irradiance data

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pandas as pd
-from datetime import datetime, timedelta
-from pytz import timezone
+from datetime import datetime
 from time import time, sleep
 import mido
 import multiprocessing as mp
 import asyncio
 import random
 from platypush.context import get_plugin
+from pytz import timezone
+import numpy as np
+
 
 def data_loop(timestamp):
     my_data_loop = Data()
@@ -52,41 +54,42 @@ class Audio:
 
 class Data:
     def __init__(self):
-        self.df = self.process_data()
-        
+
+        self.df = self.load_data()
+
         # Setup audio
         self.outport = mido.open_output('IAC Driver creatures')
 
-    def process_data(self):
+    def load_data(self):
+        df = pd.read_csv('irradiance.csv')
 
-        # Read in data and drop unecessary columns
-        df = pd.read_csv('sunrise_sunset_times.csv')
-        df.drop(columns=['Unnamed: 4', 'Daylength'], inplace=True)
+        df_mins = pd.DataFrame(data={'mins': range(60)})
 
-        # Convert sunrise and sunset times to datetime objects in Pacfic winter time
-        df['Sunrise'] = pd.to_datetime(df['Date'] + ' ' + df['Sunrise']).dt.tz_localize('US/Pacific').dt.tz_convert('Etc/GMT+8')
-        df['Sunset'] = pd.to_datetime(df['Date'] + ' ' + df['Sunset']).dt.tz_localize('US/Pacific').dt.tz_convert('Etc/GMT+8')
-        
-        # Add in twilight start and end times
-        df['Twilight Start'] = df['Sunrise'] - timedelta(hours=1, minutes=30)
-        df['Twilight End'] = df['Sunset'] + timedelta(hours=1, minutes=30)
+        # Cross-join data to expand from hourly to 1 min
+        df['key'] = 0
+        df_mins['key'] = 0
 
-        df['Date'] = pd.to_datetime(df['Date']).dt.tz_localize('US/Pacific').dt.tz_convert('Etc/GMT+8')
+        df = df.merge(df_mins, on='key', how='outer')
+
+        df['Timestamp'] = pd.to_datetime(df['Timestamp']) + pd.to_timedelta(df['mins'], unit='m')
+
+        df.drop(columns=['key', 'mins'], inplace=True)
+
+        # Apply a rolling window and average. Data is reversed to get a forward looking average.
+        df.loc[:, df.columns!='Timestamp'] = df.loc[:, df.columns!='Timestamp'][::-1].rolling(60, min_periods=1).mean()[::-1]
+
+        # Normalize entire dataset between 1.5 and 0
+        df.loc[:, df.columns!='Timestamp'] = df.loc[:, df.columns!='Timestamp'].apply(lambda x: 1.5 * (x - x.min())/(x.max() - x.min()))
+
+        #Add some noise
+        df['angle'] = 4 * (df.index % 90) * np.pi / 180
+        df['Direct Beam'] = df['Direct Beam'] + df['angle'].apply(lambda x: (np.sin(x)+1)/5)
+
+        # Normalize entire dataset between 1.5 and 0, then clip to 1
+        df.loc[:, df.columns!='Timestamp'] = df.loc[:, df.columns!='Timestamp'].apply(lambda x: 1.5 * (x - x.min())/(x.max() - x.min())).clip(upper=1)
 
 
         return df
-
-    def get_data_start_time(self):
-
-        start_time = datetime.now()
-        decimal_time = int(start_time.hour)/24 + int(start_time.minute)/(24*60) + int(start_time.second)/(24*60*60)
-
-        first_date = datetime(year=2022, month=12, day=21)
-        data_duration = (365*24*60*60)
-
-        data_start_time = first_date.timestamp() + (decimal_time * data_duration)
-
-        return datetime.fromtimestamp(data_start_time, timezone('Etc/GMT+8'))
 
 
     def convert_to_color(self, num):
@@ -101,77 +104,64 @@ class Data:
 
         return {'x':x, 'y':y}
 
-    # def get_current_timestamp(self, tz_offset=-7):
-    #     # Returns the timestep of the timeseries data based on current time of day
-    #     local_time = ((time() + (tz_offset * 60 * 60)) % SECONDS_IN_DAY)
-    #     current_timestamp = FIRST_TIMESTAMP + SECONDS_IN_YEAR * local_time/SECONDS_IN_DAY
-    #     current_timestamp = DATA_INTERVAL * int(current_timestamp/DATA_INTERVAL)
-    #     return current_timestamp
+    def get_data_start_time(self):
+
+        start_time = datetime.now()
+        decimal_time = int(start_time.hour)/24 + int(start_time.minute)/(24*60) + int(start_time.second)/(24*60*60)
+
+        first_date = datetime(year=2022, month=12, day=21)
+        data_duration = (365*24*60*60)
+
+        data_start_time = first_date.timestamp() + (decimal_time * data_duration)
+
+        return datetime.fromtimestamp(data_start_time, timezone('Etc/GMT+8'))
+
 
     def run(self, current_timestamp):
 
         # Setup time counters
         real_start_time = time()
-        data_time = self.get_data_start_time()
+        prev_color = -1
+
+        # Filter to correct point in data based on time of day
+        df = self.df[self.df['Timestamp']>np.datetime64(self.get_data_start_time())]
 
         while True:
-            for i, row in self.df.iterrows():
-                # Skip forward to start of data
-                if row['Date'].date() == data_time.date():
-                    # We're on the right line for this date
-                    while row['Date'].date() == data_time.date():
-                        print(data_time)
-                        # While this is still true...
-                        if data_time < row['Twilight Start']:
-                            # Before Twilight it is dark
-                            val = 1
-                        elif data_time < row['Sunrise']:
-                            # Between twilight start and sunrise the sun is partially up
-                            # Figure out how far we are past twilight
-                            val = (row['Sunset'].timestamp() - data_time.timestamp()) / (60*90)
+            for i, row in df.iterrows():
+                print(row['Timestamp'], row['Direct Beam'])
 
-                        elif data_time < row['Sunset']:
-                            # During the day it is light
-                            val = 0
+                msg = mido.Message('note_on', note=MIDDLE_C, velocity=64)
+                self.outport.send(msg)
 
-                        elif data_time < row['Twilight End']:
-                            # Between sunset and twilight end the sun is partially up
-                            # Figure out how far we are past twilight
-                            val = (data_time.timestamp() - row['Sunset'].timestamp()) / (60*90)
+                cc_value = random.randint(1,100)
 
-                        else:
-                            # After twilight end it is dark
-                            val = 1
+                msg = mido.Message('control_change', control=1, value=cc_value)
+                self.outport.send(msg)
+                print("Setting control signal %i to %i" % (1, cc_value))
 
-                        # Do stuff
-                        msg = mido.Message('note_on', note=MIDDLE_C, velocity=64)
-                        self.outport.send(msg)
+                msg = mido.Message('note_off', note=MIDDLE_C, velocity=64)
+                self.outport.send(msg)
 
-                        cc_value = random.randint(1,100)
+                color = self.convert_to_color(row['Direct Beam'])
 
-                        msg = mido.Message('control_change', control=1, value=cc_value)
-                        self.outport.send(msg)
-                        print("Setting control signal %i to %i" % (1, cc_value))
-
-                        msg = mido.Message('note_off', note=MIDDLE_C, velocity=64)
-                        self.outport.send(msg)
-
-                        color = self.convert_to_color(val)
+                if color != prev_color:
+                    for n in range(2):
                         try:
-                            get_plugin('zigbee.mqtt').device_set(device='Bulb 1', property='color', value=color)
-                            get_plugin('zigbee.mqtt').device_set(device='Bulb 2', property='color', value=color)
-                            print('Setting Bulb 1 to %f', color)
-                            print('Setting Bulb 2 to %f', color)
+                            bulb_name = 'Bulb ' + str(n+1)
+                            get_plugin('zigbee.mqtt').device_set(device=bulb_name, property='color', value=color)
+                            print('Setting %f to %f', bulb_name, color)
+                            #sleep(0.2)
 
                         except:
                             continue
 
-                        # Advance data time
-                        data_time = data_time + timedelta(minutes=15)
+                prev_color = color
 
-                        # Sleep until the next beat
-                        sleep((TEMPO - ((time() - real_start_time) % TEMPO))/1000000.0)
-
+                # Sleep until the next beat
+                sleep((TEMPO - ((time() - real_start_time) % TEMPO))/(15 * 1000000.0))
+            
+            # Next time round use full dataset
+            df = self.df
 
 if __name__ == "__main__":
     
