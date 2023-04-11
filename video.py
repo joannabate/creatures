f049704f5a4fe1143989ce33b274a3a43023f72b
Jon Bate <jonbate@jons-mbp-2.gateway.sonic.net>
2023-04-11 13:41:23 -0700

    video now runs faster with faster bpm

--- video.py
+++ video.py
@@ -13,30 +13,27 @@ class Video:
         self.df = df
         self.width = width
         self.height = height
-        self.font = cv2.FONT_HERSHEY_SIMPLEX
-        self.font_scale = 1
-        self.thickness = 1
         self.background_color = (0,0,0)
         self.window_name = window_name
-        self.font_color=(0,0,255)
+        self.font_color = (0,0,255)
+        self.max_fps = 40
 
         self.videos = self.get_videos()
 
         # Assume default sunset and sunrise times
         self.sunrise = 360
         self.sunset = 1080
-
         
         # Subtract a quarter because it takes a moment to load the video
         self.music_changes = [x - 1 for x in [720, 1104, 1232, 1360, 48, 176]]
 
-        self.season_end_dates = {
-            'autumn': {'date': pd.Timestamp('2022-12-21').date(), 'date_name': 'winter solstice'},
-            'winter': {'date': pd.Timestamp('2023-03-20').date(), 'date_name': 'spring equinox '},
-            'spring': {'date': pd.Timestamp('2023-06-21').date(), 'date_name': 'summer solstice'},
-            'summer': {'date': pd.Timestamp('2023-09-22').date(), 'date_name': 'autumn equinox'},
-            'autumn': {'date': pd.Timestamp('2023-12-21').date(), 'date_name': 'winter solstice'}
-        }
+        self.season_end_dates = [
+            {'season': 'autumn', 'date': pd.Timestamp('2022-12-21').date(), 'date_name': 'winter solstice'},
+            {'season': 'winter', 'date': pd.Timestamp('2023-03-20').date(), 'date_name': 'spring equinox '},
+            {'season': 'spring', 'date': pd.Timestamp('2023-06-21').date(), 'date_name': 'summer solstice'},
+            {'season': 'summer', 'date': pd.Timestamp('2023-09-22').date(), 'date_name': 'autumn equinox'},
+            {'season': 'autumn', 'date': pd.Timestamp('2023-12-21').date(), 'date_name': 'winter solstice'}
+        ]
 
     # Returns a dict of folders : filenames
     def get_videos(self, path='videos'):
@@ -73,10 +70,8 @@ class Video:
 
     def get_season(self, i):
         current_date = self.df.loc[i, "Timestamp"].date()
-        # print(current_date)
 
-        for season_name, season in self.season_end_dates.items():
-            # print(str(season_name) + ' ' + str(season['date']))
+        for season in self.season_end_dates:
             if current_date < season['date']:
                 bottom_text = "%d days until %s" % ((season['date'] - current_date).days, season['date_name'])
                 break
@@ -84,7 +79,7 @@ class Video:
                 bottom_text = season['date_name'].upper()
                 break
 
-        return season_name, bottom_text
+        return season['season'], bottom_text
 
     def get_day_segment(self, i):
             
@@ -115,7 +110,6 @@ class Video:
 
         i_last = -1
         day_segment_last = None
-        night_video = randint(0,6)
         day_segment = self.get_day_segment(i.value)
         season, bottom_text = self.get_season(i.value)
         frame_time_last = time()
@@ -140,126 +134,134 @@ class Video:
 
             frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 
+            n = 0
+            skip_frames = 1
+
             while cap.isOpened():
 
-                ret, frame = cap.read()
+                # ret, frame = cap.read()
+                ret = cap.grab()
 
                 if ret == True:
 
-                    frame_time = time()
-                    actual_fps = 1/(frame_time - frame_time_last)
-                    frame_time_last = frame_time
+                    n = n + 1
+                    if n % skip_frames == 0:
+                        
+                        _, frame = cap.retrieve() #decode frame
 
-                    print('adjusted fps: ' + str(adjusted_fps))
-                    print('actual fps: ' + str(actual_fps))
+                        frame_time = time()
+                        actual_fps = 1/(frame_time - frame_time_last)
+                        frame_time_last = frame_time
 
-                    # sleep(1/adjusted_fps)
+                        # print('adjusted fps: ' + str(adjusted_fps))
+                        # print('actual fps: ' + str(actual_fps))
+                        # print('skip frames: ' + str(skip_frames))
 
-                    sleep((1/adjusted_fps) - ((time() - start_time) % (1/adjusted_fps)))
+                        sleep((1/adjusted_fps) - ((time() - start_time) % (1/adjusted_fps)))
 
-                    if i.value != i_last: # timestep has changed
-                        # print("i = ", i.value)
-                        i_last = i.value
+                        if i.value != i_last: # timestep has changed
+                            # print("i = ", i.value)
+                            i_last = i.value
 
-                        adjusted_fps = fps * bpm.value/100
+                            adjusted_fps = fps * bpm.value/100
+                            skip_frames = int(adjusted_fps/self.max_fps) + 1
+                            adjusted_fps = adjusted_fps/skip_frames
 
-                        # Get index for hour of day
-                        day_idx = i.value % 1440
+                            # Get index for hour of day
+                            day_idx = i.value % 1440
 
-                        # Set season and bottom text
-                        season, bottom_text = self.get_season(i.value)
+                            # Set season and bottom text
+                            season, bottom_text = self.get_season(i.value)
 
-                        # If day segment has changed, break and start over
-                        if self.get_day_segment(i.value) != day_segment:
-                            day_segment_last = day_segment
-                            day_segment = self.get_day_segment(i.value)
+                            # If day segment has changed, break and start over
+                            if self.get_day_segment(i.value) != day_segment:
+                                day_segment_last = day_segment
+                                day_segment = self.get_day_segment(i.value)
 
-                            print('Day segment has changed from ' + day_segment_last + ' to ' + day_segment)
-                            # Update sunrise and sunset times
-                            if day_segment == 'sunrise' and day_segment_last == 'night':
-                                self.sunrise = day_idx
-                            elif day_segment == 'night' and day_segment_last == 'sunset':
-                                self.sunset = day_idx
+                                print('Day segment has changed from ' + day_segment_last + ' to ' + day_segment)
+                                # Update sunrise and sunset times
+                                if day_segment == 'sunrise' and day_segment_last == 'night':
+                                    self.sunrise = day_idx
+                                elif day_segment == 'night' and day_segment_last == 'sunset':
+                                    self.sunset = day_idx
 
-                            break
+                                break
 
-                        # If music has changed, change video, break and start over
-                        if day_idx in self.music_changes:
-                            print('Music has changed')
-                            night_video = choice([i for i in range(0,12) if i != night_video])
-                            break
+                            # If music has changed break and start over
+                            if day_idx in self.music_changes:
+                                print('Music has changed')
+                                break
 
-                        row = self.df.iloc[i.value]
+                            row = self.df.iloc[i.value]
 
-                        if row['Timestamp'].hour < 10:
-                            padding = 25
-                        else:
-                            padding = 0
+                            if row['Timestamp'].hour < 10:
+                                padding = 25
+                            else:
+                                padding = 0
 
-                        time_text=row['Timestamp'].strftime("%-I:%M")
-                        am_pm_text =row['Timestamp'].strftime("%p").lower()
+                            time_text=row['Timestamp'].strftime("%-I:%M")
+                            am_pm_text =row['Timestamp'].strftime("%p").lower()
 
-                        # If it's night
-                        if day_segment == 'night':
-                            # Inverse of below
-                            brightness_reduction = row['Direct Beam'] * 2 * 255
-                        else:
-                            # No reduction when brightness is 1 (i.e. midday)
-                            # full reduction when brightness is 0.5 (i.e. sunset/sunrise)
-                            brightness_reduction = (1 - (row['Direct Beam'] - 0.5) * 2) * 255
+                            # If it's night
+                            if day_segment == 'night':
+                                # Inverse of below
+                                brightness_reduction = row['Direct Beam'] * 2 * 255
+                            else:
+                                # No reduction when brightness is 1 (i.e. midday)
+                                # full reduction when brightness is 0.5 (i.e. sunset/sunrise)
+                                brightness_reduction = (1 - (row['Direct Beam'] - 0.5) * 2) * 255
+                                
+
+                        # Resize frame
+                        frame = cv2.resize(frame, (self.width, self.height))
+
+                        #Adjust brightness
+                        frame = self.change_brightness(frame, value=-brightness_reduction)
+
+                        # Add in time
+                        ft.putText(img=frame,
+                            text=time_text,
+                            org=(225+padding, 250),
+                            fontHeight=150,
+                            color=(255,  255, 255),
+                            thickness=-1,
+                            line_type=cv2.LINE_AA,
+                            bottomLeftOrigin=True)
+                        
+                        # Add in am/pm
+                        ft.putText(img=frame,
+                            text=am_pm_text,
+                            org=(325, 375),
+                            fontHeight=100,
+                            color=(255,  255, 255),
+                            thickness=-1,
+                            line_type=cv2.LINE_AA,
+                            bottomLeftOrigin=True)
+                        
+                        # Add in bottom text
+                        ft.putText(img=frame,
+                            text=bottom_text,
+                            org=(50, 550),
+                            fontHeight=50,
+                            color=(255,  255, 255),
+                            thickness=-1,
+                            line_type=cv2.LINE_AA,
+                            bottomLeftOrigin=True)
                             
 
-                    # Resize frame
-                    frame = cv2.resize(frame, (self.width, self.height))
+                        cv2.imshow(self.window_name, frame)
+                        cv2.namedWindow(self.window_name, cv2.WINDOW_FULLSCREEN)
+                        cv2.setWindowProperty(self.window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN) #Disable when on large monitor
+                        # cv2.resizeWindow(self.window_name, self.width, self.height) #Enable when on large monitor
 
-                    #Adjust brightness
-                    frame = self.change_brightness(frame, value=-brightness_reduction)
-
-                    # Add in time
-                    ft.putText(img=frame,
-                        text=time_text,
-                        org=(225+padding, 250),
-                        fontHeight=150,
-                        color=(255,  255, 255),
-                        thickness=-1,
-                        line_type=cv2.LINE_AA,
-                        bottomLeftOrigin=True)
-                    
-                    # Add in am/pm
-                    ft.putText(img=frame,
-                        text=am_pm_text,
-                        org=(325, 375),
-                        fontHeight=100,
-                        color=(255,  255, 255),
-                        thickness=-1,
-                        line_type=cv2.LINE_AA,
-                        bottomLeftOrigin=True)
-                    
-                    # Add in bottom text
-                    ft.putText(img=frame,
-                        text=bottom_text,
-                        org=(50, 550),
-                        fontHeight=50,
-                        color=(255,  255, 255),
-                        thickness=-1,
-                        line_type=cv2.LINE_AA,
-                        bottomLeftOrigin=True)
-                        
-
-                    cv2.imshow(self.window_name, frame)
-                    cv2.namedWindow(self.window_name, cv2.WINDOW_FULLSCREEN)
-                    cv2.setWindowProperty(self.window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN) #Disable when on large monitor
-                    # cv2.resizeWindow(self.window_name, self.width, self.height) #Enable when on large monitor
-
-                    k = cv2.waitKey(1)
-                    if k==27:    # Esc key to stop
-                        cap.release()
-                        cv2.destroyAllWindows()
-                        return
+                        k = cv2.waitKey(1)
+                        if k==27:    # Esc key to stop
+                            cap.release()
+                            cv2.destroyAllWindows()
+                            return
                 else:
                     print('returning to start of video')
                     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
 
 
 if __name__ == "__main__":
